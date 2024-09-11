46daa243c18273455318cab8d4d9131ff4184c16
VaibhavSawner <169150951+VaibhavSawner@users.noreply.github.com>
2024-09-11 10:57:25 +0530

    Update WithDraw.java

--- src/p1/WithDraw.java
+++ src/p1/WithDraw.java
@@ -5,7 +5,7 @@ public class WithDraw {
 	public void  wDraw(int amt)
 	{
 		System.out.println("Amount of WithDraw " +amt);
-		System.out.println("Balance amount " +(2000-amt));
+		System.out.println("Balance amount " +(8000-amt));
 		System.out.println("Transaction is succesfull");
 	}
 
